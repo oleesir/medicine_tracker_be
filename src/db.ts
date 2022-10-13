3b0eb66e8a7d424e884ab0f7d173ef9cd3c2ad0a
oleesir <oleesir@gmail.com>
2022-10-13 13:53:21 -0400

    changes

--- src/db.ts
+++ src/db.ts
@@ -5,14 +5,14 @@ dotenv.config();
 
 const {Pool} = pg;
 
-const {USER, HOST, DATABASE, PASSWORD, DBPORT } = process.env
+const {USER, HOST, DATABASE, PASSWORD, DB_PORT } = process.env
 
 const pool = new Pool({
     user: USER,
     host: HOST,
     database: DATABASE,
     password: PASSWORD,
-    port: parseInt(<string>DBPORT, 10),
+    port: parseInt(<string>DB_PORT, 10),
     idleTimeoutMillis: 0,
     connectionTimeoutMillis: 0,
 });
--- src/jobs/notificationsJob.ts
+++ src/jobs/notificationsJob.ts
@@ -5,41 +5,40 @@ import Mailer from "../utils/mailer";
 const notificationsJob = async () => {
     const today = new Date();
     const currentDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    let hours = today.getHours() <= 9 ? '0' + today.getHours()  : today.getHours() ;
+    let hours = today.getHours() <= 9 ? '0' + today.getHours() : today.getHours();
     let minutes = today.getMinutes() <= 9 ? '0' + today.getMinutes() : today.getMinutes();
 
     const currentTime = hours + ":" + minutes;
 
     await pool.query(PrescriptionQueries.updatePrescriptionStatus, [currentDate]);
 
-    const firstTimer =  await pool.query(PrescriptionQueries.getFirstTimers, [currentTime]);
-   const secondTimer =  await pool.query(PrescriptionQueries.getSecondTimers, [currentTime]);
-    const thirdTimer =  await pool.query(PrescriptionQueries.getThirdTimers, [currentTime]);
+    const firstTimer = await pool.query(PrescriptionQueries.getFirstTimers, [currentTime]);
+    const secondTimer = await pool.query(PrescriptionQueries.getSecondTimers, [currentTime]);
+    const thirdTimer = await pool.query(PrescriptionQueries.getThirdTimers, [currentTime]);
 
 
-
-firstTimer.rows.map((user)=>{
-         Mailer.send({
+    firstTimer.rows.map((user) => {
+        Mailer.send({
             to: user.email,
             subject: `Medication Reminder for ${user.first_timer}.`,
-            text:`Hey ${user.first_name} it's time to take your ${user.drug_name} medication.`
+            text: `Hey ${user.first_name} it's time to take your ${user.drug_name} medication.`
         });
     })
 
 
-    secondTimer.rows.map((user)=>{
+    secondTimer.rows.map((user) => {
         Mailer.send({
             to: user.email,
             subject: `Medication Reminder for ${user.second_timer}.`,
-            text:`Hey ${user.first_name} it's time to take your ${user.drug_name} medication.`
+            text: `Hey ${user.first_name} it's time to take your ${user.drug_name} medication.`
         });
     })
 
-    thirdTimer.rows.map((user)=>{
+    thirdTimer.rows.map((user) => {
         Mailer.send({
             to: user.email,
             subject: `Medication Reminder for ${user.third_timer}.`,
-            text:`Hey ${user.first_name} it's time to take your ${user.drug_name} medication.`
+            text: `Hey ${user.first_name} it's time to take your ${user.drug_name} medication.`
         });
     })
 
