8da2ec6a7e452fc980a0816ea53b97aeb7d77ede
oleesir <oleesir@gmail.com>
2022-10-13 14:55:52 -0400

    changes

--- src/db.ts
+++ src/db.ts
@@ -1,20 +1,28 @@
-import  pg from 'pg'
+import pg from 'pg'
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const {Pool} = pg;
+const { Pool } = pg;
 
-const {USER, HOST, DATABASE,DATABASE_URI, PASSWORD, DB_PORT } = process.env
+const { USER, HOST, DB_NAME, DB_URI, PASSWORD, DB_PORT } = process.env
+
+const config = DB_URI ?
+    {
+        connectionString: DB_URI,
+    } :
+    {
+        user: USER,
+        host: HOST,
+        database: DB_NAME,
+        password: PASSWORD,
+        port: parseInt(<string>DB_PORT, 10),
+    }
 
 const pool = new Pool({
-    user: USER,
-    host: HOST,
-    database: process.env.NODE_ENV === "development" ? DATABASE : DATABASE_URI ,
-    password: PASSWORD,
-    port: parseInt(<string>DB_PORT, 10),
-    idleTimeoutMillis: 0,
-    connectionTimeoutMillis: 0,
+        ...config,
+        idleTimeoutMillis: 0,
+        connectionTimeoutMillis: 0
 });
 
 
